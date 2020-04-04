6a5a5f2ff00446e8e80c68fa68f1f528e83265fc
Scott Campbell <stc9486@gmail.com>
2020-04-04 12:42:53 -0700

    rr

--- react-movie-fan/src/App.js
+++ react-movie-fan/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+import About from './About';
+import Home from './Home'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+        <Router>
+        <h3>hi, i'm app :)</h3>
+          <div>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+          </div>
+          <Route exact path='/about' render={props => <About /> } />
+          <Route exact path='/' component={Home} />
+        <h6>and i'm still here</h6>
+        </Router>
+
+    )
+  }
 }
-
-export default App;
